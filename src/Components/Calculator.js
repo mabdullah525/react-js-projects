69aad98fe96ec4f3c4f6c9ab367384cef841a729
mabdullah525 <abdullahbinabdullah005@gmail.com>
2025-06-15 10:29:26 -0700

    Initial commit

---
+++ src/Components/Calculator.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+
+const Calculator = () => {
+    const [result, setResult] = useState("");
+
+    const getValue = (event) => {
+        setResult(result.concat(event.target.value));
+
+    }
+    const calculation = () => {
+        setResult(eval(result).toString());
+
+    }
+    const del = () => {
+        setResult(result.slice(0, -1));
+    }
+    const ac = () => {
+        setResult("");
+    }
+
+    return (
+        <div className='container'>
+            <div className='zero'>
+                <input placeholder="0" value={result} />
+            </div>
+            <button onClick={getValue} value="(">(</button>
+            <button onClick={getValue} value=")">)</button>
+            <button onClick={getValue} value="%">%</button>
+            <button onClick={ac}>AC</button>
+
+            <button onClick={getValue} value="7">7</button>
+            <button onClick={getValue} value="8">8</button>
+            <button onClick={getValue} value="9">9</button>
+            <button onClick={getValue} value="*">*</button>
+
+            <button onClick={getValue} value="1">1</button>
+            <button onClick={getValue} value="2">2</button>
+            <button onClick={getValue} value="3">3</button>
+            <button onClick={getValue} value="+">+</button>
+
+            <button onClick={getValue} value="0">0</button>
+            <button onClick={del}>Del</button>
+            <button onClick={calculation}>=</button>
+            <button onClick={getValue} value="/">/</button>
+
+
+
+
+        </div>
+    )
+}
+
+export default Calculator
