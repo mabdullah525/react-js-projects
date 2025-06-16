a8c3255734dfa5c9d58e7c889e8bd68e36526036
mabdullah525 <abdullahbinabdullah005@gmail.com>
2025-06-16 14:11:25 -0700

    new tabs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import Calculator from './Components/Calculator';
 import Stopwatch from './Components/Stopwatch';
+import Accordian from './Components/Accordian';
 
 function App() {
   const [activeTab, setActiveTab] = useState('calculator');
@@ -10,22 +11,24 @@ function App() {
     <div className='container'>
       <div className="tab-btns">
         <button
-          className={`cal ${activeTab === 'calculator' ? 'active' : ''}`}
+          className={`${activeTab === 'calculator' ? 'active' : ''}`}
           onClick={() => setActiveTab('calculator')}
         >
           Calculator
         </button>
         <button
-          className={`stopw ${activeTab === 'stopwatch' ? 'active' : ''}`}
+          className={`${activeTab === 'stopwatch' ? 'active' : ''}`}
           onClick={() => setActiveTab('stopwatch')}
         >
           Stopwatch
         </button>
+        <button className={`${activeTab === 'accordian' ? 'active' : ''}`} onClick={() => setActiveTab('accordian')}>Accordian</button>
       </div>
 
       <div className='app'>
         {activeTab === 'calculator' && <Calculator />}
         {activeTab === 'stopwatch' && <Stopwatch />}
+        {activeTab === 'accordian' && <Accordian />}
       </div>
     </div>
   );
