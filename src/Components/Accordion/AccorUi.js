a93b11f42911eb0b8372e06a281fa9a83d34f24a
Abdullah <muhammadabdullahweb005@gmail.com>
2025-06-22 12:35:27 -0700

    Accor complete

--- src/Components/Accordion/AccorUi.js
+++ src/Components/Accordion/AccorUi.js
@@ -5,18 +5,21 @@ import '../../Css/Accor.css';
 
 
 const AccorUi = ({ curElem }) => {
-  const [data, setData] =useState(curElem);
+  const [data, setData] =useState(false);
+  const showHide = () => {
+    setData(!data);
+  }
   return (
     <>
       <div className='accordion'>
         <div className='qns'>
           <div className='qns-header'>
             <h3>{curElem.title}</h3>
-            <button>
-              {data ? <i className="fa-solid fa-plus"></i> : null}
+            <button onClick={showHide}>
+              {data ? <i className="fa-solid fa-minus"></i> : <i class="fa-solid fa-plus"></i>}
             </button>
           </div>
-          <p>{curElem.content}</p>
+          <p>{data ? curElem.content : ""}</p>
         </div>
       </div>
 
