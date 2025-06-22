4b1f11c245cd407215ddf2e8c71b3b3845f76702
Abdullah <muhammadabdullahweb005@gmail.com>
2025-06-22 14:25:29 -0700

    greeting done

--- src/Components/Greeting.js
+++ src/Components/Greeting.js
@@ -1,16 +1,47 @@
-import React from 'react'
-import '../Css/Greeting.css'
+import React, { useEffect, useState } from 'react';
+import '../Css/Greeting.css';
+import morning from "../Images/goodmorning.png";
+import afternoon from "../Images/afternoon.png";
+import evening from "../Images/evening.png";
 
 const Greeting = () => {
-  return (
-    <>
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    const currentHour = time.getHours();
+    const currentTime = time.toLocaleTimeString();
+    const isAm = time.getHours() < 12;
+
+    let greetingMessage = '';
+    let imageSource = '';
+
+    if (currentHour < 12) {
+        greetingMessage = 'Good Morning';
+        imageSource = morning;
+    } else if (currentHour < 18) {
+        greetingMessage = 'Good Afternoon';
+        imageSource = evening;
+    } else {
+        greetingMessage = 'Good Evening';
+        imageSource = afternoon;
+    }
+
+
+    return (
         <div className="greeting">
             <h2>Greeting Website</h2>
-            <h1></h1>
-            <p></p>
+            <h1>{currentTime} <span className="ampm">{isAm ? 'AM' : 'PM'}</span></h1>
+            <p>{greetingMessage}</p>
+            <img src={imageSource} alt="Time of Day" className="sun" />
         </div>
-    </>
-  )
-}
+    );
+};
 
-export default Greeting
+export default Greeting;
