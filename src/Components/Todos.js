5acaf0348a48ff7a52f6e4eb4e1084e1901d52d0
mabdullah525 <muhammadabdullahweb005@gmail.com>
2025-06-25 08:30:36 -0700

    function

--- src/Components/Todos.js
+++ src/Components/Todos.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Css/Todos.css'
 
 const Todos = () => {
+    const [input, setInput] = useState();
+    const [data, setData] = useState([]);
+
+    const getInput = (e) => {
+        setInput(e.target.value);
+    }
+    const getData = () => {
+        let store = [...data]
+        setData(input)
+    }
     return (
         <>
             <div className='todos'>
                 <div className='todo-input'>
-                    <input type="text" placeholder='Enter Your Task' />
-                    <button>Add Todo</button>
+                    <input type="text" placeholder='Enter Your Task' onChange={getInput} />
+                    <button onClick={getData}>Add Task</button>
                 </div>
+                {data}
             </div>
 
         </>
