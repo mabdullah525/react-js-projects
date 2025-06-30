08f37855c923b003b30d32feb6d263447ec9a194
mabdullah525 <muhammadabdullahweb005@gmail.com>
2025-06-30 07:01:53 -0700

    done

--- src/Components/Todos.js
+++ src/Components/Todos.js
@@ -9,17 +9,35 @@ const Todos = () => {
         setInput(e.target.value);
     }
     const getData = () => {
-        let store = [...data]
-        setData(input)
+        let store = [...data, input];
+        setData(store);
+        setInput('');
+    }
+    const deleteTask = (e) => {
+        const taskToDelete = e.target.previousSibling.innerText;
+        const updatedData = data.filter(task => task !== taskToDelete);
+        setData(updatedData);
     }
     return (
         <>
             <div className='todos'>
                 <div className='todo-input'>
-                    <input type="text" placeholder='Enter Your Task' onChange={getInput} />
+                    <input type="text" placeholder='Enter Your Task' onChange={getInput} value={input} />
                     <button onClick={getData}>Add Task</button>
                 </div>
-                {data}
+                {data.map((curVal, index) => {
+                    return (
+                        <>
+                            <div className='taskData'>
+                                <p>{curVal}</p>
+                                <i id='delIcon' onClick={deleteTask} class="fa-solid fa-trash"></i>
+
+                            </div>
+                        </>
+                    )
+
+
+                })}
             </div>
 
         </>
